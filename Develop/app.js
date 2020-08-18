7f0bbdece1d7779d346f3797a19de23793201ebf
Natalie Garcia <gnatalie.booking@gmail.com>
2020-08-18 11:22:30 -0400

    added initial readme, created basic layout for classes and function in app.js

--- Develop/app.js
+++ Develop/app.js
@@ -9,15 +9,64 @@ const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
+ class Employee {
+     constructor(name, id, email)
+     getName() {
+        
+     }
+     getID() {
+
+     }
+     getEmail() {
+
+     }
+
+     getRole() {
+         return Employee
+     }
+ }
+ class Manager extends Employee {
+     constructor(officeNumber)
+     getRole() {
+         return Manager
+     }
+
+ }
+ 
+ class Engineer extends Employee {
+     constructor(github)
+     getRole() {
+         return Engineer
+        }
+        
+    }
+    class Intern extends Employee {
+                    constructor(school)
+                
+        getSchool(){}
+        getRole() {
+            return Intern
+        }
+                }
+ 
+// const manager = new Manager()
+// const intern = new Intern()
+// const engineer = new Engineer()
 
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
 
+addTeamMember() {
+    const newManager = new Manager()
+
+}
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
 // generate and return a block of HTML including templated divs for each employee!
+generateHTML() {
 
+}
 // After you have your html, you're now ready to create an HTML file using the HTML
 // returned from the `render` function. Now write it to a file named `team.html` in the
 // `output` folder. You can use the variable `outputPath` above target this location.
--- README.md
+++ README.md
@@ -1,1 +1,33 @@
-# team-generator
+# Team Generator
+
+## Table of Contents
+* [Description](#description)
+* [Installation](#installation)
+* [Usage](#usage)
+* [Links](#links)
+* [Tests](#tests)
+* [Questions](#questions)
+
+## Description
+A comman line application that takes inputs and generates a software engineering team page with basic information and quick access to emails and Github profiles. The application uses Node.js, inquirer, fs, OOP, html, and JavaScript. 
+
+## Installation 
+To install this application you must use a command line application and install: Node.js, the inquirer package, and fs package. 
+
+## Usage
+The Team Generator application is used to make it easy to get to know the team you are working with and access contact information for each member. 
+
+## Links
+Github repo: https://github.com/nataliegarcia-8/team-generator
+
+
+
+## Tests
+test information
+
+## Questions 
+### If you have any questions please contact me at:
+Email: gnatalie.booking@gmail.com
+
+Github: https://github.com/nataliegarcia-8
+
